4c2922197303de1697694352dccdcfaf4069b933
Juan Badia Payno <jbadiapa@redhat.com>
2017-06-16 07:04:50 +0000

    Fix include done gem on doneGems list

--- helperScripts/rubygemDownloader/downloader.py
+++ helperScripts/rubygemDownloader/downloader.py
@@ -1,8 +1,9 @@
 import RubygemDownloader
 import RubygemParser
 import RubygemCBSChecker 
+import pprint 
 
-gems =  ['webmock']
+gems =  ['fluent-plugin-kubernetes_metadata_filter']
 
 gem = gems.pop(0)
 
@@ -12,6 +13,7 @@ dependencies ={}
 doneGems = []
 
 while gem:
+    print (gem)
     if (doneGems.count(gem) == 0):
         cbsChecker = RubygemCBSChecker.RubygemCBSChecker()
         if ((not cbsChecker.check('rubygem-{}'.format(gem))) and
@@ -23,20 +25,18 @@ while gem:
             runtimeDeps = runtimeDeps + parser.runtimeDeps
             devDeps = devDeps + parser.devDeps
             dependencies[gem]={'run':parser.runtimeDeps, 'dev':parser.devDeps, 'download':parser.link}
-            gems = gems + parser.runtimeDeps + parser.devDeps
+            gems = gems + parser.devDeps
         else:
             dependencies[gem]=cbsChecker.packageUrl[37:-2]
+        doneGems.append(gem)
 
     if len(gems)>0:
         gem = gems.pop(0)
     else:
         gem = None
+    with open ('fluent-plugin-kubernetes_metadata_filter','w') as out:
+        pp = pprint.PrettyPrinter(indent=1,stream=out)
+        pp.pprint(dependencies)
 
-print runtimeDeps
-print 
-print devDeps
-print 
-print doneGems
-print
-print dependencies
-
+pp = pprint.PrettyPrinter(indent=1)
+pp.pprint(dependencies)
